af5be3bcc4cd592cc5b8a3c18945d27487d9ae11
mycom <mycom@DESKTOP-PC1>
2023-02-22 23:46:43 +0900

    - second commit

--- first/src/first/hello/world/First.java
+++ first/src/first/hello/world/First.java
@@ -4,7 +4,8 @@ public class First {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("First Commit");
+		System.out.println("Second Commit");
 	}
 
 }
